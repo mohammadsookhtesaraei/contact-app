be0260e8134a9fef09360f6522dc0e261f39df40
mohammadso <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2025-05-13 12:27:32 +0330

    Update Layout.jsx

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -2,12 +2,12 @@ import styles from "./Layout.module.css"
 
 const Layout = ({children}) => {
     return (
-        <div>
+        <>
             <header className={styles.header}>
                 <h1>Contact App</h1>
             </header>
             {children}
-        </div>
+        <>
     );
 }
 
